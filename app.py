7f83d709a695203d69cc9f1a2d6841c3fe16f5fa
MAYURESH1204 <168071153+MAYURESH1204@users.noreply.github.com>
2025-06-07 19:46:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 from scipy.optimize import minimize
 
 import os
-app = Flask(__name__, template_folder=os.path.join(os.path.dirname(__file__), 'templates'))
+app = Flask(__name__, template_folder='templates'))
 
 
 @app.route('/', methods=['GET', 'POST'])
